5569aa48194b779519c7ce0c63e51b7f7d355834
Aldrin Jenson <aldrinjenson@gmail.com>
2022-03-06 09:37:33 +0530

    Add signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import "./signup.css";
+import { Autocomplete } from "@mui/material";
+import banks from "../../utils/banksList";
+import { Box } from "@mui/system";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { fireStoreDb } from "../../fbConfig";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const handleSubmit = (e) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bank, setBank] = useState();
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [upiId, setUpiId] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("hi");
+    const auth = getAuth();
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const obj = {
+        email,
+        name,
+        password,
+        bank,
+        phoneNumber,
+        upiId,
+        userId: user.uid,
+      };
+      await setDoc(doc(fireStoreDb, "users", user.uid), obj);
+      alert("Successfully signed up✨");
+      navigate("/");
+    } catch (error) {
+      console.log("Error in signing up: " + error);
+      alert(
+        "There seems to be some error in signing up. Please verify your inputs and try again."
+      );
+    }
   };
   return (
     <div className="signup">
@@ -14,16 +55,12 @@ const Signup = () => {
         <form onSubmit={handleSubmit}>
           <div>
             <TextField
-              label="First Name"
+              label="Name"
               variant="outlined"
               style={{ width: "25em" }}
-            />
-            <br />
-            <br />
-            <TextField
-              label="Last Name"
-              variant="outlined"
-              style={{ width: "25em" }}
+              value={name}
+              requried={true}
+              onChange={(e) => setName(e.target.value)}
             />
             <br />
             <br />
@@ -31,6 +68,26 @@ const Signup = () => {
               label="Phone Number"
               variant="outlined"
               style={{ width: "25em" }}
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <br />
+            <br />
+            <TextField
+              label="Email"
+              variant="outlined"
+              style={{ width: "25em" }}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <br />
+            <TextField
+              label="Password"
+              variant="outlined"
+              style={{ width: "25em" }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <br />
             <br />
@@ -38,18 +95,46 @@ const Signup = () => {
               label="UPI ID"
               variant="outlined"
               style={{ width: "25em" }}
+              value={upiId}
+              onChange={(e) => setUpiId(e.target.value)}
             />
             <br />
             <br />
-            <TextField
-              label="Bank"
-              variant="outlined"
-              style={{ width: "25em" }}
+            <Autocomplete
+              id="country-select-demo"
+              options={banks}
+              autoHighlight
+              value={bank}
+              isOptionEqualToValue={() => bank.label}
+              onChange={(e, newVal) => setBank(newVal)}
+              getOptionLabel={(option) => option.label}
+              renderOption={(props, option) => (
+                <Box
+                  component="li"
+                  sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                  {...props}
+                >
+                  <img width="40" src={option.imgUrl} alt="bank logo" />
+                  {option.label}
+                </Box>
+              )}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Choose your bank"
+                  inputProps={{
+                    ...params.inputProps,
+                    // autoComplete: "new-password", // disable autocomplete and autofill
+                  }}
+                />
+              )}
             />
             <br />
             <br />
             <div style={{ textAlign: "center" }}>
-              <Button variant="contained">Sign Up</Button>
+              <Button type="submit" variant="contained">
+                Sign Up
+              </Button>
             </div>
           </div>
         </form>
--- src/utils/banksList.js
+++ src/utils/banksList.js
@@ -1,17 +1,68 @@
-const banks =[
- { imgUrl: "https://www.examsegg.com/wp-content/uploads/2016/02/examsegg1.png", name: "Examsegg Learning" },​
- { imgUrl: "https://www.examsegg.com/wp-content/uploads/2017/10/national-banks-in-india.jpg", name: "How Many National Banks are in India?" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/allahabad-bank-300x92.jpg", name: "Allahabad Bank" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/andhra-bank-300x88.jpg", name: "Andhra Bank" },​
- { imgUrl: "https://www.examsegg.com/wp-content/uploads/2017/10/bank-of-baroda-e1603855805123-150x150.jpg", name: "Bank of Baroda" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/bank-of-india.jpg", name: "Bank of India" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/bank-of-maharashtra-300x91.jpg", name: "Bank of Maharashtra" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/Canara-banks-300x78.gif", name: "Canara Bank" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/central-bank-of-india-300x98.png", name: "Central Bank of India" },​
- { imgUrl: "https://www.examsegg.com/wp-content/uploads/2017/10/Corpbank-150x150.jpg", name: "Corporation Bank" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/dena-bank-logo-300x154.jpg", name: "Dena Bank" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/Indian-Bank-300x77.jpg", name: "Indian Bank" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/indian-overseas-bank-300x54.png", name: "Indian Overseas Bank" },​
- { imgUrl: "https://content.examsegg.com/uploads/2017/10/oriental-bank-of-commerce-300x164.jpg", name: "Oriental Bank of Commerce" },​
-]
+const banks = [
+  {
+    imgUrl:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Fl3gDe2i78C1d2s-V5MIgwHaE8%26pid%3DApi&f=1",
+    label: "State Bank of India(SBI)",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/allahabad-bank-300x92.jpg",
+    label: "Allahabad Bank",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/andhra-bank-300x88.jpg",
+    label: "Andhra Bank",
+  },
+  {
+    imgUrl:
+      "https://www.examsegg.com/wp-content/uploads/2017/10/bank-of-baroda-e1603855805123-150x150.jpg",
+    label: "Bank of Baroda",
+  },
+  {
+    imgUrl: "https://content.examsegg.com/uploads/2017/10/bank-of-india.jpg",
+    label: "Bank of India",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/bank-of-maharashtra-300x91.jpg",
+    label: "Bank of Maharashtra",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/Canara-banks-300x78.gif",
+    label: "Canara Bank",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/central-bank-of-india-300x98.png",
+    label: "Central Bank of India",
+  },
+  {
+    imgUrl:
+      "https://www.examsegg.com/wp-content/uploads/2017/10/Corpbank-150x150.jpg",
+    label: "Corporation Bank",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/dena-bank-logo-300x154.jpg",
+    label: "Dena Bank",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/Indian-Bank-300x77.jpg",
+    label: "Indian Bank",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/indian-overseas-bank-300x54.png",
+    label: "Indian Overseas Bank",
+  },
+  {
+    imgUrl:
+      "https://content.examsegg.com/uploads/2017/10/oriental-bank-of-commerce-300x164.jpg",
+    label: "Oriental Bank of Commerce",
+  },
+];
+
 export default banks;
